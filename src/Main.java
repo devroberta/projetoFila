a94af9fc5276d01d90591a4f4baba4be737ad570
Roberta de Siqueira <designer.roberta@gmail.com>
2023-03-07 15:02:42 -0300

    project FIFO

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue(new No("Primeiro"));
+        minhaFila.enqueue(new No("Segundo"));
+        minhaFila.enqueue(new No("Terceiro"));
+        minhaFila.enqueue(new No("Quarto"));
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.dequeue());
+
+        System.out.println(minhaFila);
+
+        minhaFila.enqueue(new No("Ultimo"));
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.first());
+
+        System.out.println(minhaFila);
+    }
+}
